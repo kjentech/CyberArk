67bf28aed958eceacfa6678720366ff6564b92ea
kjentech <kasper.jensen1473@gmail.com>
2021-12-01 14:44:24 +0100

    finally ready to be published :)

--- ModifyPlatforms.ps1
+++ ModifyPlatforms.ps1
@@ -23,12 +23,14 @@ $props = [ordered]@{
     MinLowerCase                  = "2"
     MinDigit                      = "1"
     MinSpecial                    = "1"
-    PasswordForbiddenChars        = @("'", '´', '`', '^', '~', 'i', 'l', 'o', '0')            # rule of thumb: use single-quotes, only double-quote when enclosing a single-quote
+    PasswordForbiddenChars        = '''´`^~ilo0'            # single-quotes inside single-quote strings are escaped with single-quotes, so ' ''asd ' will output 'asd, rest are interpreted literally. https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_quoting_rules?view=powershell-7.2
     #PasswordEffectiveLength = "16"
+
+
 
     ###### Change Task
     AllowManualChange             = "Yes"
-    PerformPeriodicChange         = "No"
+    #PerformPeriodicChange         = "No"
     #HeadStartInterval = "5"                                # In days (0 for none)
     #ChangeNotificationPeriod = "-1"                        # Minimum number of seconds the change is delayed to allow application password provider synchronization. Use -1 or comment the parameter for no notification
     #DaysNotifyPriorExpiration = "7"
@@ -38,7 +40,7 @@ $props = [ordered]@{
 
     ###### Verification Task
     VFAllowManualVerification     = "Yes"
-    VFPerformPeriodicVerification = "No"
+    #VFPerformPeriodicVerification = "No"
     #VFFromHour = "-1"                                        # Expected values: 0-23 or -1 for none
     #VFToHour = "-1"                                          # Expected values: 0-23 or -1 for none
     #VFExecutionDays = "Mon,Tue,Wed,Thu,Fri,Sat,Sun"          # one string, seperated by comma
@@ -75,7 +77,7 @@ $props = [ordered]@{
 # set $UsePACLI = $false to skip all PACLI functions.
 # this script will then just edit the INI files placed in $tempPath, you can upload them to Vault yourself using PrivateArk Client
 
-$UsePACLI = $true
+$UsePACLI = $false
 $PACLIPath = "C:\Install\Core Pas 12\PACLI-Rls-v12.0"
 $Vault = "LabVault"
 $VaultIP = "172.20.65.10"
@@ -87,99 +89,266 @@ $FolderName = "Root\Policies"
 ###########
 
 
-
-
 function editFile {
-    foreach ($file in (dir $tempPath)) {
-
-        
-        #$contentList = [System.Collections.Generic.List[string]]::new()
-        $iniHashtable = [ordered]@{}
-
-        # edit
-        #try {
-            #$file = "Policy-P-WIN-DOM-15.ini"
-            cd $tempPath
-            #Get-Content $file | foreach {$contentList.Add($_)}
-
-            $contentList = Get-Content $file | foreach {
-                 [PSCustomObject]@{
-                    Name = $_.Split("=")[0]
-                    Value = $_.Split("=")[1]
+    begin {
+        function Get-Ini {
+            [CmdletBinding()]
+            param (
+                [IO.FileInfo]$FilePath = "$tempPath\$file",
+                [switch]$IgnoreComments,
+                [string]$CommentChar = ";",
+                [string]$NoSection = "_"
+            )
+    
+    
+            $ini = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+    
+            $commentRegex = "^\s*([$($CommentChar -join '')].*)$"
+            $sectionRegex = "^\s*\[(.+)\]\s*$"
+            $keyRegex = "^\s*(.+?)\s*=\s*(['`"]?)(.*)\2\s*$"
+    
+    
+            $commentCount = 0
+            switch -regex -file $FilePath {
+                $sectionRegex {
+                    # Section
+                    $section = $matches[1]
+                    Write-Verbose "$($MyInvocation.MyCommand.Name):: Adding section : $section"
+                    $ini[$section] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+                    $CommentCount = 0
+                    continue
                 }
-            }
-            
-
-            # first filter - only get key-value pairs before a Section marker ( [...] )
-            
-            $stop = $false
-            foreach ($line in $contentList) {
-                if ($line.Name -match "\[") { $stop = $true }
-                if ($stop -ne $true) {
-                    $currentKeyValue = $line.Split("=")
-                    $iniHashtable[$currentKeyValue[0]] = $currentKeyValue[1]
-
-                    foreach ($Targetkey in $props.Keys) {
-                        if ($currentKey -match $Targetkey) {
-                            #$appendList.add("$Targetkey=$($props[$Targetkey])")
-                            #$keylist.add($Targetkey)
-
-
-                            #$appendHashtable[$Targetkey] = ($props[$Targetkey])
-                            #$line -replace "$Targetkey=.*","$Targetkey=$($props[$Targetkey])"
+                $commentRegex {
+                    # Comment
+                    if (!$IgnoreComments) {
+                        if (!($section)) {
+                            $section = $NoSection
+                            $ini[$section] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+                        }
+                        $value = $matches[1]
+                        $CommentCount++
+                        Write-Debug ("Incremented CommentCount is now {0}." -f $CommentCount)
+                        $name = "Comment" + $CommentCount
+                        Write-Verbose "$($MyInvocation.MyCommand.Name):: Adding $name with value: $value"
+                        $ini[$section][$name] = $value
+                    }
+                    else {
+                        Write-Debug ("Ignoring comment {0}." -f $matches[1])
+                    }
+    
+                    continue
+                }
+                $keyRegex {
+                    # Key
+                    if (!($section)) {
+                        $section = $NoSection
+                        $ini[$section] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+                    }
+                    $name, $value = $matches[1, 3]
+                    Write-Verbose "$($MyInvocation.MyCommand.Name):: Adding key $name with value: $value"
+                    if (-not $ini[$section][$name]) {
+                        $ini[$section][$name] = $value
+                    }
+                    else {
+                        if ($ini[$section][$name] -is [string]) {
+                            $ini[$section][$name] = [System.Collections.ArrayList]::new()
+                            $null = $ini[$section][$name].Add($ini[$section][$name])
+                            $null = $ini[$section][$name].Add($value)
+                        }
+                        else {
+                            $null = $ini[$section][$name].Add($value)
                         }
                     }
-
-
-                    # ;**************************************
-                    # ;New values automatically added from script
-                    # ;**************************************
-                }
-            }#foreach
-
-
-            $iniHashtable.keys | where {$_ -match $contentlist}
-            $contentList | where {$_ -match $iniHashtable[$($_.split("=")[0])]}
-
-            $props.Keys | foreach {
-                
-                [PSCustomObject]@{
-                    Name = Value
+                    continue
                 }
             }
-
-            $props.keys | foreach {$iniHashtable.remove($_)}
-            [string[]]$b = $iniHashtable.keys | foreach {
-                "$_=$($iniHashtable[$_])"
-            }
-
-            $b += ";**************************************"
-            $b += ";New values automatically added from script"
-            $b += ";**************************************"
-
-
-
-            $content | Select-String -Pattern $appendList -notmatch
-            $content = $content -notlike "*$keylist*"
-            
-
-
-            $content = $content -replace "PasswordLength=.*", "PasswordLength=$PasswordLength"
-            $content = $content -replace "MinUpperCase=.*", "MinUpperCase=$MinUpperCase"
-            $content = $content -replace "MinLowerCase=.*", "MinLowerCase=$MinLowerCase"
-            $content = $content -replace "MinDigit=.*", "MinDigit=$MinDigit"
-            $content = $content -replace "MinSpecial=.*", "MinSpecial=$MinSpecial"
-            $content = $content -replace "PasswordForbiddenChars=.*", "PasswordForbiddenChars=$($PasswordForbiddenChars -join ',')"
-            if (!($content -match "PasswordForbiddenChars")) { $content = $content -replace "MinSpecial=.*", "MinSpecial=$MinSpecial`r`nPasswordForbiddenChars=$($PasswordForbiddenChars -join ',')" }
-            Set-Content -Path $file -Value $content -Force -ErrorAction Stop
-
-            Write-Verbose "[+] File $file has been SUCCESSFULLY modified" -Verbose
+            Write-Output $ini
         }
-        catch {
-            Write-Warning "[-] File $file FAILED to be modified"
-            continue
-        } #trycatch
-    }# foreach
+    
+    
+    
+        function Set-Ini {
+            [CmdletBinding()]
+            param (
+                [Hashtable]$NameValuePairs,
+                [string]$Sections = "_",
+                #$FilePath
+
+                [Parameter( Mandatory = $true, ValueFromPipeline = $true )]
+                [System.Collections.IDictionary]
+                $InputObject
+            )
+    
+        
+    
+            begin {
+                $content = Get-Ini -FilePath $FilePath
+    
+                Function Update-IniEntry {
+                    param ($content, $section)
+        
+                    foreach ($pair in $NameValuePairs.GetEnumerator()) {
+                        if (!($content[$section])) {
+                            Write-Verbose ("$($MyInvocation.MyCommand.Name):: '{0}' section does not exist, creating it." -f $section)
+                            $content[$section] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+                        }
+        
+                        Write-Verbose ("$($MyInvocation.MyCommand.Name):: Setting '{0}' key in section {1} to '{2}'." -f $pair.key, $section, $pair.value)
+                        $content[$section][$pair.key] = $pair.value
+                    }
+                }
+            }
+    
+            process {
+                if ($Sections) {
+                    foreach ($section in $Sections) {
+                        # Get rid of whitespace and section brackets.
+                        $section = $section.Trim() -replace '[][]', ''
+                        Update-IniEntry $content $section
+                    }
+                }
+                else {
+                    # No section supplied, go through the entire ini since changes apply to all sections.
+                    foreach ($item in $content.GetEnumerator()) {
+                        $section = $item.key
+                        Update-IniEntry $content $section
+                    }
+                }
+                Write-Output $content
+            }
+        }
+    
+    
+    
+        function Out-Ini {
+            [CmdletBinding()]
+            param (
+                [ValidateNotNullOrEmpty()][ValidateScript( { Test-Path $_ -IsValid })]
+                [Parameter( Mandatory, ValueFromPipelineByPropertyName )]
+                [string]$FilePath,
+    
+                [Parameter( Mandatory = $true, ValueFromPipeline = $true )]
+                [System.Collections.IDictionary]
+                $InputObject,
+    
+                [ValidateSet("Unicode", "UTF7", "UTF8", "ASCII", "BigEndianUnicode", "Byte", "String")]
+                [string]$Encoding = "UTF8",
+    
+                [string]$NoSection = "_"
+            )
+    
+            begin {
+                function Out-Keys {
+                    param(
+                        [ValidateNotNullOrEmpty()]
+                        [Parameter( Mandatory, ValueFromPipeline )]
+                        [System.Collections.IDictionary]$InputObject,
+    
+                        [ValidateNotNullOrEmpty()][ValidateScript( { Test-Path $_ -IsValid })]
+                        [Parameter( Mandatory, ValueFromPipelineByPropertyName )]
+                        [string]$FilePath,
+    
+                        [ValidateSet("Unicode", "UTF7", "UTF8", "ASCII", "BigEndianUnicode", "Byte", "String")]
+                        [Parameter( Mandatory )]
+                        [string]$Encoding = "UTF8",
+    
+                        $Delimiter = "=",
+    
+                        [Parameter( Mandatory )]
+                        $MyInvocation
+                    )
+    
+                    Process {
+                        if (!($InputObject.get_keys())) {
+                            Write-Warning ("No data found in '{0}'." -f $FilePath)
+                        }
+                        Foreach ($key in $InputObject.get_keys()) {
+                            if ($key -match "^Comment\d+") {
+                                Write-Verbose "$($MyInvocation.MyCommand.Name):: Writing comment: $key"
+                                "$($InputObject[$key])" | Out-File -Encoding $Encoding -FilePath $FilePath -Append
+                            }
+                            else {
+                                Write-Verbose "$($MyInvocation.MyCommand.Name):: Writing key: $key"
+                                $InputObject[$key] |
+                                ForEach-Object { "$key$delimiter$_" } |
+                                Out-File -Encoding $Encoding -FilePath $FilePath -Append
+                            }
+                        }
+                    }
+                }
+    
+                $Delimiter = "="
+    
+                # Splatting Parameters
+                $parameters = @{
+                    Encoding = $Encoding;
+                    FilePath = $FilePath
+                }
+            }
+    
+            process {
+                $extraLF = ""
+                $outFile = New-Item -ItemType file -Path $Filepath -Force
+                if (!(Test-Path $outFile.FullName)) { Throw "Could not create File" }
+    
+                Write-Verbose "$($MyInvocation.MyCommand.Name):: Writing to file: $Filepath"
+                foreach ($i in $InputObject.get_keys()) {
+                    if (!($InputObject[$i].GetType().GetInterface('IDictionary'))) {
+                        #Key value pair
+                        Write-Verbose "$($MyInvocation.MyCommand.Name):: Writing key: $i"
+                        "$i$delimiter$($InputObject[$i])" | Out-File -Append @parameters
+    
+                    }
+                    elseif ($i -eq $NoSection) {
+                        #Key value pair of NoSection
+                        Out-Keys $InputObject[$i] `
+                            @parameters `
+                            -Delimiter $delimiter `
+                            -MyInvocation $MyInvocation
+                    }
+                    else {
+                        #Sections
+                        Write-Verbose "$($MyInvocation.MyCommand.Name):: Writing Section: [$i]"
+    
+                        # Only write section, if it is not a dummy ($script:NoSection)
+                        if ($i -ne $NoSection) { "$extraLF[$i]"  | Out-File -Append @parameters }
+    
+                        if ( $InputObject[$i].Count) {
+                            Out-Keys $InputObject[$i] `
+                                @parameters `
+                                -Delimiter $delimiter `
+                                -MyInvocation $MyInvocation
+                        }
+    
+                    }
+                }
+                Write-Verbose "$($MyInvocation.MyCommand.Name):: Finished Writing to file: $FilePath"
+    
+            }
+    
+    
+    
+        }
+    }
+
+    process {
+        foreach ($file in (dir $tempPath)) {
+            try {
+                $stripped = Get-Ini -FilePath $file
+                $props.keys | foreach { $stripped["_"].remove($_) }
+                $c = $stripped["_"] + $props
+                $d = Get-Ini -FilePath $file | Set-Ini -NameValuePairs $c
+                $d | Out-Ini -FilePath "$($file.BaseName)-new.ini"
+
+                Write-Verbose "[+] File $file has been SUCCESSFULLY modified" -Verbose
+
+            }
+            catch {
+                Write-Warning "[-] File $file FAILED to be modified"
+                continue
+            } #trycatch
+        }# foreach
+    }# process
 }
 
 function PACLIsetup {
